bfedb3105835031ef0aba6207f29464d0e046e63
metherul <greenmarleyethan@gmail.com>
2020-05-13 16:30:48 -0400

    Implemented test for IArchiveEntryFactory

--- src/Gearbox.SDK/Index/Factories/ArchiveEntryFactory.cs
+++ src/Gearbox.SDK/Index/Factories/ArchiveEntryFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Threading.Tasks;
 using SystemHandle.AsyncFilesystem;
@@ -13,28 +14,30 @@ namespace Gearbox.Sdk.Index.Factories
     {
         private readonly IAsyncArchive _asyncArchive;
         private readonly IAsyncDirectory _asyncDirectory;
-        private readonly IAsyncHash _hashers;
+        private readonly IFileInfoFactory _fileInfoFactory;
+        private readonly IAsyncHash _asyncHash;
         private readonly IFileEntryFactory _fileEntryFactory;
 
         public ArchiveEntryFactory(IFileEntryFactory fileEntryFactory, IAsyncArchive archiveHandle,
-            IAsyncDirectory asyncDirectory, IAsyncHash hashers)
+            IAsyncDirectory asyncDirectory, IFileInfoFactory fileInfoFactory, IAsyncHash asyncHash)
         {
             _fileEntryFactory = fileEntryFactory;
             _asyncArchive = archiveHandle;
             _asyncDirectory = asyncDirectory;
-            _hashers = hashers;
+            _fileInfoFactory = fileInfoFactory;
+            _asyncHash = asyncHash;
         }
 
         public async Task<IArchiveEntry> Create(string archivePath)
         {
-            var archiveInfo = new FileInfo(archivePath);
+            var archiveInfo = _fileInfoFactory.FromFileName(archivePath);
             var archiveName = archiveInfo.Name;
 
             // Extract the contents of the archive to a temp directory.
             var extractDir = Path.Combine(Path.GetTempPath(), Path.GetFileNameWithoutExtension(archiveName));
 
             // Here we begin extracting the archive and process each file as its decompressed.
-            var fileEntries = new List<FileEntry>();
+            var fileEntries = new List<IFileEntry>();
             var entryTasks = new List<Task<IFileEntry>>();
             
             await foreach (var file in _asyncArchive.ExtractAndYieldOutput(archivePath, extractDir))
@@ -43,7 +46,6 @@ namespace Gearbox.Sdk.Index.Factories
                 entryTasks.Add(fileEntry);
             }
             
-            await _asyncArchive.Extract(extractDir, archivePath);
             await Task.WhenAll(entryTasks);
 
             var archiveEntry = new ArchiveEntry()
@@ -51,8 +53,8 @@ namespace Gearbox.Sdk.Index.Factories
                 Name = archiveName,
                 ArchivePath = archivePath,
                 LastModified = archiveInfo.LastWriteTimeUtc,
-                FilesystemHash = await _hashers.MakeFilesystemHash(extractDir),
-                Hash = await _hashers.MakeMd5(archivePath),
+                FilesystemHash = await _asyncHash.MakeFilesystemHash(extractDir),
+                Hash = await _asyncHash.MakeMd5(archivePath),
                 FileEntries = entryTasks.Select(x => x.Result).ToList(),
                 Length = archiveInfo.Length
             };
---
+++ src/Gearbox.Sdk.Tests/ArchiveEntryFactoryTests.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Reflection;
+using System.Threading.Tasks;
+using SystemHandle.AsyncFilesystem;
+using Gearbox.Sdk.Index.Factories;
+using Gearbox.Sdk.Index.Models;
+using Gearbox.Sdk.Index.Writer;
+using Moq;
+using NUnit.Framework;
+
+namespace Gearbox.Sdk.Tests
+{
+    public class ArchiveEntryFactoryTests
+    {
+        public async IAsyncEnumerable<string> GetArchiveContentsTestData()
+        {
+            var archiveContents = new List<string>
+            {
+                "file.txt",
+                "file2.txt",
+                "dir1/file.txt",
+                "dir2/file.txt",
+                "dir3/dir4/file.txt",
+                "dir3/dir4/file2.txt"
+            };
+
+            foreach (var file in archiveContents)
+            {
+                yield return file;
+            }
+        }
+        
+        [Test]
+        public async Task TestCreate_ShouldPass()
+        {
+            var archiveContents = new List<string>
+            {
+                "file.txt",
+                "file2.txt",
+                "dir1/file.txt",
+                "dir2/file.txt",
+                "dir3/dir4/file.txt",
+                "dir3/dir4/file2.txt"
+            };
+            
+            var archiveName = "TestArchive.7z";
+            var lastWriteTime = DateTime.UtcNow;
+            
+            var fileInfoMock = new Mock<IFileInfo>();
+            fileInfoMock.Setup(x => x.Name).Returns(archiveName);
+            fileInfoMock.Setup(x => x.LastWriteTimeUtc).Returns(lastWriteTime);
+            fileInfoMock.Setup(x => x.Length).Returns(1);
+                
+            var fileInfoFactoryMock = new Mock<IFileInfoFactory>();
+            fileInfoFactoryMock
+                .Setup(x => x.FromFileName(It.IsAny<string>()))
+                .Returns(fileInfoMock.Object);
+
+            var asyncArchiveMock = new Mock<IAsyncArchive>();
+            asyncArchiveMock
+                .Setup(x => x.ExtractAndYieldOutput(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(GetArchiveContentsTestData);
+            
+            var fileEntryFactoryMock = new Mock<IFileEntryFactory>();
+            fileEntryFactoryMock
+                .Setup(x => x.Create(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<FileHashType>()))
+                .ReturnsAsync(It.IsAny<IFileEntry>());
+
+            var asyncHashMock = new Mock<IAsyncHash>();
+            asyncHashMock.Setup(x => x.MakeFilesystemHash(It.IsAny<string>())).ReturnsAsync(It.IsAny<string>());
+            asyncHashMock.Setup(x => x.MakeMd5(It.IsAny<string>())).ReturnsAsync(It.IsAny<string>());
+
+            var asyncDirectoryMock = new Mock<IAsyncDirectory>();
+            asyncDirectoryMock.Setup(x => x.DeleteAsync(It.IsAny<string>(), It.IsAny<bool>()));
+
+            var archiveEntryFactory = new ArchiveEntryFactory(fileEntryFactoryMock.Object,
+                asyncArchiveMock.Object,
+                asyncDirectoryMock.Object,
+                fileInfoFactoryMock.Object,
+                asyncHashMock.Object);
+
+            var result = await archiveEntryFactory.Create(archiveName);
+            
+            // Test to ensure every call to IFileEntryFactory was to a file in the archive.
+            fileEntryFactoryMock.Verify(x => x.Create(It.Is<string>(a => archiveContents.Contains(a)), It.IsAny<string>(), It.IsAny<FileHashType>()));
+            
+            // Test to ensure every call to IAsyncHashFactory was to a file in the archive.
+            asyncHashMock.Verify(x => x.MakeMd5(It.Is<string>(a => a == archiveName)));
+            
+            // Test to ensure that the result contains the correct file entries and archive information.
+            Assert.AreEqual(result.Name, archiveName);
+            Assert.AreEqual(result.ArchivePath, archiveName);
+            Assert.AreEqual(result.Length, fileInfoMock.Object.Length);
+            Assert.AreEqual(result.LastModified, lastWriteTime);
+            Assert.AreEqual(result.FileEntries.Count, archiveContents.Count);
+        }
+    }
+}
